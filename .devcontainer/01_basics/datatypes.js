64975acfc14512bae8ff86e325eecddeb260bb68
Sahil1262 <152700841+Sahil1262@users.noreply.github.com>
2025-02-01 09:35:53 +0000

    datatypes

---
+++ .devcontainer/01_basics/datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat js code as a new version
+
+alert(3+3) // we are using nodejs not browser 
+
+let name = "sahil"
+let age = 18
+let isLoggedIn= false
+let state;
+
+//datatypes 
+
+/*
+number -> 2 to power 53
+bigInt
+string => " "
+boolean=> true/ false
+null => standalon value
+undefined 
+symbol => unique
+*/
+
+//objects
+console.log(typeof null)// object
+console.log(typeof undefined)// undefined
